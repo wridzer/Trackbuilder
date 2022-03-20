35e1aa8a8569019718ccf6d6c5689f76e4b730b6
Wridzer Kamphuis <wridzer12@gmail.com>
2022-03-20 11:13:40 +0100

    started on UI and finite state machine

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+public abstract class State<T>
+{
+    Type commandType { get; set; }
+
+    GameObject prefab { get; set; }
+
+    State(Type _commandType, GameObject _prefab)
+    {
+        commandType = _commandType;
+        prefab = _prefab;
+    }
+
+    public abstract void OnEnter();
+    public abstract void OnUpdate();
+    public abstract void OnExit();
+}
---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class StateMachine<T>
+{
+    private State<T> currentState;
+    private Dictionary<System.Type, State<T>> allStates = new Dictionary<System.Type, State<T>>();
+
+    public T pOwner
+    {
+        get;
+        protected set;
+    }
+
+    public StateMachine(T _owner)
+    {
+        pOwner = _owner;
+    }
+
+    public void AddState(State<T> _State)
+    {
+        allStates.Add(_State.GetType(), _State);
+    }
+
+    public void Update()
+    {
+        currentState?.OnUpdate();
+    }
+
+    public void SwitchState(System.Type _type)
+    {
+        currentState?.OnExit();
+        if (allStates.ContainsKey(_type))
+        {
+            currentState = allStates[_type];
+        }
+        currentState?.OnEnter();
+    }
+}
